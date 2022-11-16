ca1b08daf61bf8983b59d85b59220e74f6ed542c
ridhorayputraa <ridhorayputra@gmail.com>
2022-11-16 19:29:10 +0700

    after layouting system

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'title' => 'Home'
+    ]);
 });
 
 
 
 Route::get('/about', function () {
     return view('about',[
+        'title' => 'About',
         'name' => "Ridho Ray",
         'email' => 'RIdhoray033@gmail.com',
         'img' => 'ridho.png'
@@ -30,5 +33,7 @@ Route::get('/about', function () {
 
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts', [
+        'title' => 'Posts'
+    ]);
 });
