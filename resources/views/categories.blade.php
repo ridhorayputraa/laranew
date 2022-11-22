ab0046840f70e440ba3a5c432acab39c6fca8389
ridhorayputraa <ridhorayputra@gmail.com>
2022-11-22 20:50:09 +0700

    addig category models

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -7,7 +7,7 @@
 @foreach ($categories as $category)
 <ul>
     <li>
-        <h2> <a href="/categories/{{ $category->slug }}">{{ $category->name }}</a></h2>
+        <h1> <a href="/categories/{{ $category->slug }}">{{ $category->name }}</a></h1>
     </li>
 </ul>
 @endforeach
