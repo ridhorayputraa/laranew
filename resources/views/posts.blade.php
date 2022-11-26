52174494f806bbe3f5353918966b60fc358aba4c
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-11-26 18:27:13 +0700

    adding more detail & category user in Posts Page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,13 +2,21 @@
 @extends('layouts.main')
 
 @section('container')
+<h1 class="mb-5">Halaman Blog Posts</h1>
 
 @foreach ($posts as $post)
-<article class="mb-5">
+<article class="mb-5 border-bottom pb-4">
     <h2>
-        <a href="/post/{{ $post->slug}}">{{ $post->title }}</a></h2>
-    <h5>By: {{ $post->author }}</h5>
+        <a class="text-decoration-none" href="/post/{{ $post->slug}}">{{ $post->title }}</a>
+    </h2>
+
+        {{-- <h5>By: {{ $post->author }}</h5> --}}
+    <p>By Ridho ray in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
+
     <p>{{ $post->excerpt }}</p>
+
+    <a class="text-decoration-none" href="/post/{{ $post->slug}}">Read More..</a>
+
 </article>
 @endforeach
 
