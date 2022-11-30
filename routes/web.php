0ff6bb41a03d3ae64ed529e3e364377986bc974c
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-11-30 22:08:35 +0700

    updatingn contrller

--- routes/web.php
+++ routes/web.php
@@ -75,6 +75,6 @@ Route::get('/categories/{category:slug}', function(Category $category){
 Route::get('/author/{author:username}', function(User $author){
     return view('posts', [
         'title' =>'Post By Author : ' . $author->name,
-        'posts' => $author->post,
+        'posts' => $author->post->load('category', 'author'),
      ]);
 });
