602d52794fba0d153572714a7ddd00b782a09137
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-01 23:17:05 +0700

    adding check if there is a posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,14 +4,24 @@
 @section('container')
 <h1 class="mb-5">{{ $title }}</h1>
 
+{{-- cek postingannya ada ga --}}
+{{-- dipakai ketika fitur pencarian --}}
+@if ($posts->count())
+{{-- hitung jumlah dari postingannya --}}
 <div class="card mb-3">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
   </div>
-</div>
+@else
+<p>Not Post Found.</p>
+
+
+@endif
+
 
 @foreach ($posts as $p)
 <article class="mb-5 border-bottom pb-4">
