47f0ff32d0a4e45c4c21526c08eb0291fd15fda5
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-02 14:21:29 +0700

    newing post views

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,13 +2,30 @@
 
 @section('container')
 
-    <h1 class="mb-5">{{ $post->title }}</h1>
+<div class="container">
+    <div class="row justify-content-center mb-5">
+        <div class="col-md-8">
+
+
+
+    <h1 class="mb-3">{{ $post->title }}</h1>
+
     <p> <a class="text-decoration-none" href="/author/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}</a></p>
 
-{!! $post->body !!}
+    <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+
+    <article class="my- fs-6">
+        {!! $post->body !!}
+    </article>
 
 
 
 
-<a href="/blog" class="d-block mt-3">Back to post</a>
+        <a href="/blog" class="d-block mt-3">Back to post</a>
+
+        </div>
+    </div>
+</div>
+
+
 @endsection
