c768f9d44f284c44074c9786c55b6270b2240355
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-02 14:26:23 +0700

    editing the new post view

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,7 +10,7 @@
 
     <h1 class="mb-3">{{ $post->title }}</h1>
 
-    <p> <a class="text-decoration-none" href="/author/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}</a></p>
+    <p>By. <a class="text-decoration-none" href="/author/{{ $post->author->username }}"> {{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}</a></p>
 
     <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
 
