a223727c8bf36f20a9d7e2aa6604a8b8da92ae78
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-02 14:11:50 +0700

    editing current view

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.main')
 
 @section('container')
-<article class="mt-5">
-    <h2>{{ $post->title }}</h2>
+
+    <h1 class="mb-5">{{ $post->title }}</h1>
     <p> <a class="text-decoration-none" href="/author/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}</a></p>
 
 {!! $post->body !!}
 
 
-</article>
 
-<a href="/blog" class="d-block mt-5">Back to post</a>
+
+<a href="/blog" class="d-block mt-3">Back to post</a>
 @endsection
