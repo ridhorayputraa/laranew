257e8a096ff4e24f6cf2d3a32bcc8714024b562d
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-03 07:21:24 +0700

    creating image overlay cards in categories

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -4,12 +4,26 @@
 @section('container')
 <h1 class="mb-5">Post Category : {{ $categories[0]->name }}</h1>
 
-@foreach ($categories as $category)
-<ul>
-    <li>
-        <h1> <a href="/categories/{{ $category->slug }}">{{ $category->name }}</a></h1>
-    </li>
-</ul>
-@endforeach
+
+<div class="container">
+    <div class="row">
+        @foreach ($categories as $category)
+
+        <div class="col-md-4">
+            <div class="card text-bg-dark">
+                <img src="https://source.unsplash.com/500x400?{{ $category->name }}"  class="card-img" alt="{{ $category->name }}">
+                <div class="card-img-overlay">
+                  <h5 class="card-title">{{ $category->name }}</h5>
+                </div>
+              </div>
+        </div>
+
+        @endforeach
+
+    </div>
+</div>
+
+
+
 
 @endsection
