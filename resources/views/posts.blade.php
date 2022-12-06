f6b4743b8c88179e4ce430527d0fdb2723285000
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-06 21:04:56 +0700

    finishing styling for search components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,10 +2,10 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="mb-5">{{ $title }}</h1>
+<h1 class="mb-3 text-center">{{ $title }}</h1>
 
 
-<div class="row">
+<div class="row justify-content-center mb-3">
     <div class="col-md-6">
         <form action="/posts">
         {{-- Tambahkan input disini --}}
