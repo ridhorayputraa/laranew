6c851b55a4b06a21e3ef9a431716c4fcf4f90cc2
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-09 23:04:21 +0700

    finishing for single posts file

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,17 +1,31 @@
 <?php
 namespace App\Http\Controllers;
+
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\User;
 
 class PostController extends Controller
 {
     //
     public function index(){
 
+        $title = '';
+        if(request('category')){
+            $category = Category::firstWhere('slug', request('category'));
+            $title = 'in ' . $category->name;
+        }
+
+
+        if(request('author')){
+            $author = User::firstWhere('username', request('author'));
+            $title = 'in ' . $author->name;
+        }
 
     return view('posts', [
         'active' => 'posts',
-        'title' => 'All Posts',
+        'title' => 'All Posts' . $title,
 
 
         // filter() dapeti dari model post hasil dari scopeFilter
