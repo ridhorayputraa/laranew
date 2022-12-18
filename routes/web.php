6f88f8b0584d5c403eab1923698ea985af255ebe
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-18 18:10:14 +0700

    initialize new controller authenticate

--- routes/web.php
+++ routes/web.php
@@ -78,6 +78,7 @@ return view('categories', [
 
 // Routes Login
 Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
 
