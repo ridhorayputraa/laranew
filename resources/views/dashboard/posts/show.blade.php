0a0211f39293f6cd2ae83a45f16a69e69c9d9ec8
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-20 16:56:57 +0700

    editing to whow image in  dashboard

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -9,9 +9,14 @@
 
     <h1 class="mb-3">{{ $post->title }}</h1>
 
-    <p>By. <a class="text-decoration-none" href="/posts?author={{ $post->author->username }}"> {{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}</a></p>
+<a href="" class="btn btn-success"  ><span data-feather="arrow-left"></span> Back to all my posts</a>
 
-    <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+<a href="" class="btn btn-warning"  ><span data-feather="edit"></span>Edit</a>
+
+<a href="" class="btn btn-danger"  ><span data-feather="edit"></span>Delete</a>
+
+
+    <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
 
     <article class="my- fs-6">
         {!! $post->body !!}
