5e4e8c49ab13cc041aa80d02d4b06134d5ff1847
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-23 16:47:49 +0700

    adding box category in view creaet

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -23,6 +23,17 @@
 
         </div>
 
+        <div class="mb-3">
+          <label for="category" class="form-label">Category</label>
+
+          <select class="form-select" name="category_id">
+               @foreach ($categories as $category )
+               <option value="{{ $category->id }}">{{ $category->name }}</option>
+
+               @endforeach
+          </select>
+        </div>
+
 
         <button type="submit" class="btn btn-primary">Create Post</button>
       </form>
