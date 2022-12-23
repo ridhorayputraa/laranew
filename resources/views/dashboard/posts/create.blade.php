f94b4bcfb464a855099b55ca042cbb665117655e
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-23 14:54:46 +0700

    create form title and slug

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -7,22 +7,24 @@
 
   {{-- form --}}
   <div class="col-lg-8">
-    
-      <form>
+
+      <form method="post" action="/dashboard/posts">
+        @csrf
+        {{-- akan langung ke method source --}}
         <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+          <label for="title" class="form-label">Title</label>
+          <input type="text" name="title" class="form-control" id="title">
+
         </div>
+
         <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1">
+          <label for="slug" class="form-label">Slug</label>
+          <input type="text" name="slug" class="form-control" id="slug">
+
         </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+
+
+        <button type="submit" class="btn btn-primary">Create Post</button>
       </form>
 
   </div>
