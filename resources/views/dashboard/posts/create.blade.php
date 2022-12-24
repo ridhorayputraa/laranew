f7dd85cf5367f541f669afd732acbad3e7f60fba
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-24 13:34:04 +0700

    adding archived old value

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -15,7 +15,7 @@
           <label for="title" class="form-label">Title</label>
           <input type="text" name="title" class="form-control @error('title')
            is-invalid
-          @enderror" id="title" autofocus>
+          @enderror" id="title" autofocus value="{{ old('title') }}">
 
           @error('title')
              <div class="invalid-feedback">
@@ -29,7 +29,7 @@
           <label for="slug" class="form-label">Slug</label>
           <input type="text" name="slug" class="form-control @error('slug')
                is-invalid
-          @enderror" disabled readonly id="slug">
+          @enderror" disabled readonly id="slug" value="{{ old('slug') }}">
           @error('slug')
                <div class="invalid-feedback">
                 {{ $message }}
@@ -41,8 +41,11 @@
                   <label for="category" class="form-label">Category</label>
                   <select class="form-select" name="category_id">
                        @foreach ($categories as $category )
+                       @if (old('category_id') == $category->id)
+                       <option value="{{ $category->id }}" selected>{{ $category->name }} </option>
+                       @else
                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-
+                       @endif
                        @endforeach
                   </select>
                 </div>
@@ -53,7 +56,7 @@
                   @error('body')
                   <p class="text-danger">{{ $message }}</p>
                   @enderror
-                  <input id="body" type="hidden" name="body">
+                  <input id="body" type="hidden" name="body" value="{{ old('body') }}">
                   <trix-editor input="body"></trix-editor>
 
                 </div>
