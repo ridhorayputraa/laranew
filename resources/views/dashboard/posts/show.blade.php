80cfb084e262617a90b938e61c8b04336c5a0974
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-25 14:19:27 +0700

    adding delete feautre to index dashboard posts and shwoing detail post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -1,4 +1,4 @@
-@dd($post)
+
 @extends('dashboard.layouts.main')
 
 @section('container')
@@ -14,8 +14,11 @@
 
 <a href="" class="btn btn-warning"  ><span data-feather="edit"></span>Edit</a>
 
-<a href="" class="btn btn-danger"  ><span data-feather="edit"></span>Delete</a>
-
+<form action="/dashboard/posts/{{ $post->slug }}" class="d-inline" method="post">
+    @method('delete')
+@csrf
+<button  class="btn btn-danger" onclick="return confirm('Are you sure?')" >Delete<span data-feather='x-circle' ></span></button>
+</form>
 
     <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
 
