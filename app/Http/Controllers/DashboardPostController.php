a8effb83c18e62b86ded70cdbe41fb1e03cf50a9
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-25 14:40:15 +0700

    validation slug

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -114,6 +114,19 @@ class DashboardPostController extends Controller
     public function update(Request $request, Post $post)
     {
         //
+        $rules = [
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required'
+        ];
+
+        // validasi untuk slug
+        // pengecualian validasi slu => LOGIC
+        if($request->slug != $post->slug){
+            $rules['slug'] = 'required|unique:posts';
+        }
+
+        $validatedData = $request->validate($rules);
     }
 
     /**
