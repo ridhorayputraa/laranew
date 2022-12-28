e2e7b69b2e1528a31f4d9040b7f2834cc491dd9b
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-28 23:58:43 +0700

    adding logic to gate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,8 @@ class AppServiceProvider extends ServiceProvider
         //
         Paginator::useBootstrap();
         Gate::define('admin', function(User $user){
-
+         return $user->username === 'ridhorayy';
+            
         });
     }
 }
