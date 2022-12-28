556cbe97aea6fccc6808fe98a23181e05486ae8e
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-28 23:31:35 +0700

    create new middleware Is Admin

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -2,41 +2,38 @@
 
 @section('container')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">My Posts</h1>
+    <h1 class="h2">Post Categories</h1>
   </div>
 
   @if (session()->has('success'))
-  <div class="alert alert-success col-lg-8" role="alert">
+  <div class="alert alert-success col-lg-6" role="alert">
     {{ session('success') }}
   </div>
   @endif
 
-  <div class="table-responsive col-lg-8">
+  <div class="table-responsive col-lg-6">
     {{-- Tombol Create post  --}}
-    <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create new post</a>
+    <a href="/dashboard/categories/create" class="btn btn-primary mb-3">Create new category</a>
     <table class="table table-striped table-sm">
       <thead>
         <tr>
           <th scope="col">#</th>
-          <th scope="col">Title</th>
-          <th scope="col">Category</th>
+          <th scope="col">Category name</th>
           <th scope="col">Action</th>
         </tr>
       </thead>
       <tbody>
-        @foreach ($posts as $post )
+        @foreach ($categories as $category )
 
 
         <tr>
             {{-- lop iteration dari laravel --}}
             <td>{{ $loop->iteration }}</td>
-
-            <td>{{ $post->title }}</td>
-            <td>{{ $post->category->name }}</td>
+            <td>{{ $category->name }}</td>
             <td>
-                <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info" ><span data-feather='eye' ></span></a>
-                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning" ><span data-feather='edit' ></span></a>
-                <form action="/dashboard/posts/{{ $post->slug }}" class="d-inline" method="post">
+                <a href="/dashboard/categories/{{ $category->slug }}" class="badge bg-info" ><span data-feather='eye' ></span></a>
+                <a href="/dashboard/categories/{{ $category->slug }}/edit" class="badge bg-warning" ><span data-feather='edit' ></span></a>
+                <form action="/dashboard/categories/{{ $category->slug }}" class="d-inline" method="post">
                     @method('delete')
                 @csrf
                 <button  class="badge bg-danger border-0" onclick="return confirm('Are you sure?')" ><span data-feather='x-circle' ></span></button>
