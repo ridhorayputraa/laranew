5cacc8608b39285fd4d6993f303fbe04ca393e2a
ridhorayputraa <ridhorayputra033@Gmail.com>
2022-12-28 23:01:57 +0700

    init data controller for sending data to view

--- app/Http/Controllers/AdminCategoryController.php
+++ app/Http/Controllers/AdminCategoryController.php
@@ -15,7 +15,9 @@ class AdminCategoryController extends Controller
     public function index()
     {
         //
-        return view('dashboard.categories.index');
+        return view('dashboard.categories.index', [
+            'categories' => Category::all()
+        ]);
     }
 
     /**
